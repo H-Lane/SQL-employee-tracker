f336e0a4f7da2d789ae48290b0ba718f7d87a0ef
Hersey <helane1996@gmail.com>
2024-08-06 16:45:13 -0400

    Added last inquirer prompts for updating

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function init() {
               "INSERT INTO department (name) VALUES (?)",
               [response.department],
               (err, data) => {
-                // if (err) console.log(err);
+                if (err) throw err;
                 console.log(`${response.department} added to Departments.`);
                 init();
               }
@@ -132,13 +132,161 @@ function init() {
                 "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
                 [response.role, response.salary, department.id],
                 (err, data) => {
-                  //   if (err) throw err;
+                  if (err) throw err;
                   console.log(`Added ${response.role} to roles.`);
                   init();
                 }
               );
             });
         });
+      } else if (response.prompt === "Add An Employee") {
+        db.query(`SELECT * FROM employee, role`, (err, result) => {
+          if (err) throw err;
+
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "firstName",
+                message: "Employee's first name:",
+                validate: (firstNameInput) => {
+                  if (firstNameInput) {
+                    return true;
+                  } else {
+                    console.log("Please Add A First Name!");
+                    return false;
+                  }
+                },
+              },
+              {
+                type: "input",
+                name: "lastName",
+                message: "What is the employees last name?",
+                validate: (lastNameInput) => {
+                  if (lastNameInput) {
+                    return true;
+                  } else {
+                    console.log("Please Add A Salary!");
+                    return false;
+                  }
+                },
+              },
+              {
+                // Adding Employee Role
+                type: "list",
+                name: "role",
+                message: "What is the employees role?",
+                choices: () => {
+                  var array = [];
+                  for (var i = 0; i < result.length; i++) {
+                    array.push(result[i].title);
+                  }
+                  var newArray = [...new Set(array)];
+                  return newArray;
+                },
+              },
+              {
+                // Adding Employee Manager
+                type: "input",
+                name: "manager",
+                message: "Who is the employees manager?",
+                validate: (managerInput) => {
+                  if (managerInput) {
+                    return true;
+                  } else {
+                    console.log("Please Add A Manager!");
+                    return false;
+                  }
+                },
+              },
+            ])
+            .then((response) => {
+              for (let i = 0; i < result.length; i++) {
+                if (result[i].title === response.role) {
+                  let role = result[i];
+                }
+              }
+
+              db.query(
+                `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+                [
+                  response.firstName,
+                  response.lastName,
+                  role.id,
+                  response.manager.id,
+                ],
+                (err, result) => {
+                  if (err) throw err;
+                  console.log(
+                    `Added ${response.firstName} ${response.lastName} to the database.`
+                  );
+                  init();
+                }
+              );
+            });
+        });
+      } else if (response.prompt === "Change Employee Role") {
+        db.query(`SELECT * FROM employee, role`, (err, result) => {
+          if (err) throw err;
+
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "employee",
+                message: "Which employees role do you want to update?",
+                choices: () => {
+                  let array = [];
+                  for (let i = 0; i < result.length; i++) {
+                    array.push(result[i].last_name);
+                  }
+                  let employeeArray = [...new Set(array)];
+                  return employeeArray;
+                },
+              },
+              {
+                type: "list",
+                name: "role",
+                message: "What is their new role?",
+                choices: () => {
+                  let array = [];
+                  for (let i = 0; i < result.length; i++) {
+                    array.push(result[i].title);
+                  }
+                  let newArray = [...new Set(array)];
+                  return newArray;
+                },
+              },
+            ])
+            .then((response) => {
+              for (let i = 0; i < result.length; i++) {
+                if (result[i].last_name === response.employee) {
+                  var name = result[i];
+                }
+              }
+
+              for (let i = 0; i < result.length; i++) {
+                if (result[i].title === response.role) {
+                  var role = result[i];
+                }
+              }
+
+              db.query(
+                `UPDATE employee SET ? WHERE ?`,
+                [{ role_id: role }, { last_name: name }],
+                (err, result) => {
+                  if (err) throw err;
+                  console.log(
+                    `Changed ${response.employee}'s role in the database`
+                  );
+                  init();
+                }
+              );
+            });
+        });
+      } else if (response.prompt === "Exit") {
+        db.end();
+        console.log("Good-Bye!");
       }
     });
 }
